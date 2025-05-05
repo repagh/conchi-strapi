5895a06cf674eb832aafba2bd77545ac85786ad0
Rene van Paassen <rene.vanpaassen@gmail.com>
2025-05-06 00:04:29 +0200

    missing data

---
+++ src/api/fecha/controllers/fecha.ts
@@ -0,0 +1,7 @@
+/**
+ * fecha controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::fecha.fecha');
---
+++ src/api/fecha/routes/fecha.ts
@@ -0,0 +1,7 @@
+/**
+ * fecha router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::fecha.fecha');
